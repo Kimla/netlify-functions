9324c560a31deef7c2f28e20fde5960b3f1e4ae1
Kim Larsson <kimla89@gmail.com>
2018-10-12 22:36:16 +0200

    first commit

---
+++ src/components/Form.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="send">Button</button>
+    <h1 v-if="message">Server says: {{ message }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: () => ({
+    message: null,
+  }),
+  methods: {
+    async send() {
+      const res = await axios.get('.netlify/functions/hello');
+      this.message = res.data;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
