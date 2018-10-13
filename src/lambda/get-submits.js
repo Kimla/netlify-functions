5d8288d249be8862e0220f9d5b56f8b99d8788c8
Kim Larsson <kimla89@gmail.com>
2018-10-13 15:39:21 +0200

    Some error management in get-submits

--- src/lambda/get-submits.js
+++ src/lambda/get-submits.js
@@ -4,11 +4,26 @@ const { MongoClient } = require('mongodb');
 const DB_URL = process.env.DB_URL;
 const DB_NAME = process.env.DB_NAME;
 
+function errorResponse(callback, err) {
+  console.error(err);
+
+  callback(null, {
+    statusCode: 500,
+    body: JSON.stringify({ error: err })
+  })
+}
+
 exports.handler = function(event, context, callback) {
   MongoClient.connect(`${DB_URL}/${DB_NAME}`, (err, connection) => {
+    if (err) return errorResponse(callback, err);
+  
     const db = connection.db(DB_NAME);
     const collection = db.collection('submits');
     collection.find({}).toArray(function(err, docs) {
+      if (err) return errorResponse(callback, err);
+
+      connection.close();
+
       callback(null, {
         statusCode: 200,
         body: JSON.stringify(docs)
